b1e1c3160b07bc2b8784a18a65c55b2009a27a32
swapnilendole02 <swapnilendole02@gmail.com>
2024-07-29 23:13:03 +0530

    Update hwp.java

--- hwp.java
+++ hwp.java
@@ -3,5 +3,6 @@ class Test
     public static void main(String []args)
     {
         System.out.println("My First Java Program.");
+        System.out.println("Execute the jenkins practical");
     }
 };
